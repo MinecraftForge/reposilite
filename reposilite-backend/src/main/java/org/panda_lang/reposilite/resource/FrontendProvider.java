15f1eee292268e7b2f0226c6c7572c349c3bb86c
LexManos <LexManos@gmail.com>
2021-04-21 16:29:46 -0700

    Add the ability to override index.html and app.js, as well as a custom 404 page.

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/resource/FrontendProvider.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/resource/FrontendProvider.java
@@ -16,33 +16,48 @@
 
 package org.panda_lang.reposilite.resource;
 
+import org.apache.http.HttpStatus;
 import org.panda_lang.reposilite.config.Configuration;
 import org.panda_lang.reposilite.utils.FilesUtils;
 import org.panda_lang.utilities.commons.StringUtils;
 import org.panda_lang.utilities.commons.function.Lazy;
 import org.panda_lang.utilities.commons.text.Formatter;
 
+import java.io.File;
 import java.util.function.Supplier;
 
 public final class FrontendProvider {
 
+    private final Formatter formatter;
     private final Lazy<String> index;
     private final Lazy<String> app;
+    private final Lazy<String> missing;
 
-    private FrontendProvider(Supplier<String> index, Supplier<String> app) {
+    private FrontendProvider(Formatter formatter, Supplier<String> index, Supplier<String> app, Supplier<String> missing) {
+        this.formatter = formatter;
         this.index = new Lazy<>(index);
         this.app = new Lazy<>(app);
+        this.missing = new Lazy<>(() -> {
+            String data = missing.get();
+            return data != null ? data : this.index.get();
+        });
     }
 
-    public String forMessage(String message) {
-        return StringUtils.replace(index.get(), "{{REPOSILITE.MESSAGE}}", message);
+    public String forMessage(int status, String message) {
+        String ret = null;
+        if (status == HttpStatus.SC_NOT_FOUND) {
+            ret = missing.get();
+        } else {
+            ret = index.get();
+        }
+        return StringUtils.replace(ret, "{{REPOSILITE.MESSAGE}}", message);
     }
 
     public String getApp() {
         return app.get();
     }
 
-    public static FrontendProvider load(Configuration configuration) {
+    public static FrontendProvider load(Configuration configuration, File workingDirectory) {
         Formatter formatter = new Formatter()
                 .register("{{REPOSILITE.BASE_PATH}}", configuration.basePath)
                 .register("{{REPOSILITE.VUE_BASE_PATH}}", configuration.basePath.substring(0, configuration.basePath.length() - 1))
@@ -51,8 +66,14 @@ public final class FrontendProvider {
                 .register("{{REPOSILITE.ACCENT_COLOR}}", configuration.accentColor);
 
         return new FrontendProvider(
-                () -> formatter.format(FilesUtils.getResource("/static/index.html")),
-                () -> formatter.format(FilesUtils.getResource("/static/js/app.js"))
+                formatter,
+                () -> formatter.format(FilesUtils.getResource("/static/index.html", workingDirectory)),
+                () -> formatter.format(FilesUtils.getResource("/static/js/app.js", workingDirectory)),
+                () -> {
+                    if (workingDirectory != null && new File(workingDirectory, "/static/404.html").exists())
+                        return formatter.format(FilesUtils.getResource("/static/404.html", workingDirectory));
+                    return null;
+                }
         );
     }
 
